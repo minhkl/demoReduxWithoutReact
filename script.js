68393e3ffb0ec2b51bfdac527520417a481dd80e
Minh Luong <luongkienminh@gmail.com>
2017-10-06 15:00:57 +0700

    apply redux

--- script.js
+++ script.js
@@ -1,17 +1,64 @@
-let state = {balance: 0}
-
 function updateBalance() {
-    document.getElementById('balance').innerHTML = '$' + state.balance
+    document.getElementById('balance').innerHTML = '$' + store.getState().balance
 }
 
 function deposit(value) {
-    state.balance += value
-    updateBalance()
+    if (value == 10) {
+        store.dispatch(actionDeposit10)
+    } else if (value == 1) {
+        store.dispatch(actionDeposit1)
+    }
 }
 
 function withdraw(value) {
-    if (state.balance >= value) {
-        state.balance -= value
+    if (value == 10) {
+        store.dispatch(actionWithdraw10)
+    } else if (value == 1) {
+        store.dispatch(actionWithdraw1)
     }
-    updateBalance()
 }
+
+// Actions
+const actionDeposit1 = {
+    type: 'DEPOSIT',
+    amount: 1
+}
+const actionDeposit10 = {
+    type: 'DEPOSIT',
+    amount: 10
+}
+const actionWithdraw1 = {
+    type: 'WITHDRAW',
+    amount: 1
+}
+const actionWithdraw10 = {
+    type: 'WITHDRAW',
+    amount: 10
+}
+
+// Reducer
+const initialState = {balance: 0}
+function reducer(state = initialState, action) {
+    switch(action.type) {
+        case 'DEPOSIT': {
+            const newBalance = state.balance + action.amount
+            return {balance: newBalance}
+        }
+        case 'WITHDRAW': {
+            if (state.balance >= action.amount) {
+                const newBalance = state.balance - action.amount
+                return {balance: newBalance}
+            } else {
+                return state
+            }
+        }
+        default:
+            return state
+    }
+}
+
+// Store
+const store = Redux.createStore(reducer)
+// const store = Redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// Register listener
+store.subscribe(updateBalance)
